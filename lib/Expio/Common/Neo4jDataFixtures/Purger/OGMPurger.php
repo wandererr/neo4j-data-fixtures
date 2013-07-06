a4a03541bc543ba609981ce26aeb4194b0c0ebe6
Simon Garner <sgarner@expio.co.nz>
2013-07-07 00:29:01 +1200

    Get the Purger class working in a barebones way

--- lib/Expio/Common/Neo4jDataFixtures/Purger/OGMPurger.php
+++ lib/Expio/Common/Neo4jDataFixtures/Purger/OGMPurger.php
@@ -31,18 +31,8 @@ use Doctrine\ORM\Mapping\ClassMetadata;
  */
 class OGMPurger implements PurgerInterface
 {
-    const PURGE_MODE_DELETE = 1;
-    const PURGE_MODE_TRUNCATE = 2;
-
     /** EntityManager instance used for persistence. */
     private $em;
-
-    /**
-     * If the purge should be done through DELETE or TRUNCATE statements
-     *
-     * @var int
-     */
-    private $purgeMode = self::PURGE_MODE_DELETE;
 
     /**
      * Construct new purger instance.
@@ -52,27 +42,6 @@ class OGMPurger implements PurgerInterface
     public function __construct(EntityManager $em = null)
     {
         $this->em = $em;
-    }
-
-    /**
-     * Set the purge mode
-     *
-     * @param $mode
-     * @return void
-     */
-    public function setPurgeMode($mode)
-    {
-        $this->purgeMode = $mode;
-    }
-
-    /**
-     * Get the purge mode
-     *
-     * @return int
-     */
-    public function getPurgeMode()
-    {
-        return $this->purgeMode;
     }
 
     /**
@@ -98,102 +67,6 @@ class OGMPurger implements PurgerInterface
     /** @inheritDoc */
     public function purge()
     {
-        $classes = array();
-        $metadatas = $this->em->getMetadataFactory()->getAllMetadata();
-
-        foreach ($metadatas as $metadata) {
-            if ( ! $metadata->isMappedSuperclass) {
-                $classes[] = $metadata;
-            }
-        }
-
-        $commitOrder = $this->getCommitOrder($this->em, $classes);
-
-        // Drop association tables first
-        $orderedTables = $this->getAssociationTables($commitOrder);
-
-        // Get platform parameters
-        $platform = $this->em->getConnection()->getDatabasePlatform();
-
-        // Drop tables in reverse commit order
-        for ($i = count($commitOrder) - 1; $i >= 0; --$i) {
-            $class = $commitOrder[$i];
-
-            if (($class->isInheritanceTypeSingleTable() && $class->name != $class->rootEntityName)
-                || $class->isMappedSuperclass) {
-                continue;
-            }
-
-            $orderedTables[] = $class->getQuotedTableName($platform);
-        }
-
-        foreach($orderedTables as $tbl) {
-            if ($this->purgeMode === self::PURGE_MODE_DELETE) {
-                $this->em->getConnection()->executeUpdate("DELETE FROM " . $tbl);
-            } else {
-                $this->em->getConnection()->executeUpdate($platform->getTruncateTableSQL($tbl, true));
-            }
-        }
-    }
-
-    private function getCommitOrder(EntityManager $em, array $classes)
-    {
-        $calc = new CommitOrderCalculator;
-
-        foreach ($classes as $class) {
-            $calc->addClass($class);
-
-            // $class before its parents
-            foreach ($class->parentClasses as $parentClass) {
-                $parentClass = $em->getClassMetadata($parentClass);
-
-                if ( ! $calc->hasClass($parentClass->name)) {
-                    $calc->addClass($parentClass);
-                }
-
-                $calc->addDependency($class, $parentClass);
-            }
-
-            foreach ($class->associationMappings as $assoc) {
-                if ($assoc['isOwningSide']) {
-                    $targetClass = $em->getClassMetadata($assoc['targetEntity']);
-
-                    if ( ! $calc->hasClass($targetClass->name)) {
-                        $calc->addClass($targetClass);
-                    }
-
-                    // add dependency ($targetClass before $class)
-                    $calc->addDependency($targetClass, $class);
-
-                    // parents of $targetClass before $class, too
-                    foreach ($targetClass->parentClasses as $parentClass) {
-                        $parentClass = $em->getClassMetadata($parentClass);
-
-                        if ( ! $calc->hasClass($parentClass->name)) {
-                            $calc->addClass($parentClass);
-                        }
-
-                        $calc->addDependency($parentClass, $class);
-                    }
-                }
-            }
-        }
-
-        return $calc->getCommitOrder();
-    }
-
-    private function getAssociationTables(array $classes)
-    {
-        $associationTables = array();
-
-        foreach ($classes as $class) {
-            foreach ($class->associationMappings as $assoc) {
-                if ($assoc['isOwningSide'] && $assoc['type'] == ClassMetadata::MANY_TO_MANY) {
-                    $associationTables[] = $assoc['joinTable']['name'];
-                }
-            }
-        }
-
-        return $associationTables;
+        throw new \Exception('Purging has not been implemented. Use append mode');
     }
 }
